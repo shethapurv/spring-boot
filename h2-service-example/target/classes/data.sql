e0ff16f15fa900edde161a015286824601461ac8
shethaptech <99895155+shethaptech@users.noreply.github.com>
2022-03-21 00:12:40 +0800

    Not required.

--- h2-service-example/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO employee (id, name) VALUES (1, 'Apurv');
-INSERT INTO employee (id, name) VALUES (2, 'Paras');
-INSERT INTO employee (id, name) VALUES (3, 'Mital');
-INSERT INTO employee (id, name) VALUES (4, 'Nischay');
-INSERT INTO employee (id, name) VALUES (5, 'Dev');
